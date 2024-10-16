07b13617bb9d552bde0b7611536322bee5b31e9b
ABdulaziz <a.captain.b@gmail.com>
2024-10-15 19:51:45 -0600

    card and bhs model added

--- server/src/models/BHSAccount.js
+++ server/src/models/BHSAccount.js
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
-export const CardSchema = new Schema(
+export const BHSSchema = new Schema(
     {
         name: { type: String, required: true },
-        digits: { type: String, required: true },
-        cvv: { type: Number, },
-        dob: { type: String },
-        pin: { type: Number },
-        notes: { type: String },
+        description: { type: String, required: true },
+        account: { type: String, required: true },
+        pass: { type: String, },
+        class: { type: String },
         creatorId: { type: Schema.Types.ObjectId, ref: 'Profile', required: true }
     },
     { timestamps: true, toJSON: { virtuals: true } }
 )
 
-CardSchema.virtual('creator', {
+BHSSchema.virtual('creator', {
     localField: 'creatorId',
     foreignField: '_id',
     justOne: true,
